c0f7a8a3088ebfadbb7233512a04a57a0c1fbe97
Evan Carlson <evanrc1@gmail.com>
2024-02-28 22:47:09 -0800

    did some debuggin and ran the build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,13 +2,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
   return {
-    mode: 'development',
+    mode: isProduction ? 'production' : 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -19,8 +22,8 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './client/index.html',
-        title: 'wired-docs',
+        template: './src/index.html',
+        filename: 'index.html', // Output the file
       }),
       new WebpackPwaManifest({
         fingerprints: false,
@@ -40,9 +43,14 @@ module.exports = () => {
         ],
       }),
       new InjectManifest({
-        swSrc: './src/js-sw.js',
+        swSrc: './src/src-sw.js',
         swDest: 'service-worker.js',
-      })
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'src/manifest.json', to: 'manifest.json' } // Copies manifest from src to dist 
+        ],
+      }),
     ],
 
     module: {
